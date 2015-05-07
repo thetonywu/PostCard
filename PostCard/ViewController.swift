47ae3a421bf4c681e06b6c3b3ee08bb8eaad60cf
Tony Wu <tonywu@twitter.com>
2015-05-06 17:19:37 -0700

    bla

--- PostCard/ViewController.swift
+++ PostCard/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
-        // some comment BLA
+        // some comment BLA BLA
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
